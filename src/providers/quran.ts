f788049ed351bb4b8eb152e165bc3d77a06c6b4e
Muhammad Fahreza <Muhammad Fahreza>
2016-12-29 08:00:58 +0700

    New Writing

--- src/providers/quran.ts
+++ src/providers/quran.ts
@@ -33,7 +33,7 @@ export class Quran {
       // We're using Angular Http provider to request the data,
       // then on the response it'll map the JSON data to a parsed JS object.
       // Next we process the data and resolve the promise with the new data.
-      this.http.get('assets/data/yaya2.txt').subscribe((res) => {
+      this.http.get('assets/data/yaya5.txt').subscribe((res) => {
       // this.http.get('assets/data/quran.csv').subscribe((res) => {
         // we've got back the raw data, now generate the core schedule data
         // and save the data for later reference
